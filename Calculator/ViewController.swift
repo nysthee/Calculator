d1e9ee3c95a3c290e3edaa5693b15fe68abbc1de
Thomas Nys <nysthee@gmail.com>
2016-02-07 23:31:28 +0100

    WIP

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     
-    var userIsInTheMiddleOfTypingANumber: Bool = false
+    var userIsInTheMiddleOfTypingANumber = false
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
@@ -20,6 +20,40 @@ class ViewController: UIViewController {
         } else {
             display.text = digit
             userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+        case "×":
+            if (operandStack.count>=2) {
+                displayValue = operandStack.removeLast() * operandStack.removeLast()
+                enter()
+            }
+//        case "÷":
+//        case "+":
+//        case "−":
+        default: break
+        }
+    }
+    var operandStack = Array<Double>()
+    @IBAction func enter(sender: AnyObject) {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        print("operandstack =\(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
         }
     }
 }
